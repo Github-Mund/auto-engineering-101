ec96abf6280e21ba0f6d51cc16db47868f428ba0
GitKraken-Mund <moralesraymundc@gmail.com>
2024-09-10 16:28:40 +0800

    Challenge 2

---
+++ chapters/ch1/second_program.py
@@ -0,0 +1,6 @@
+
+year_season = input("Enter season of the year: ")
+num_choice = input("Enter a number: ")
+adj_choice = input("Enter an adjective: ")
+
+print(f'On a {adj_choice} {year_season} day, I drink at least {num_choice} cups of juice!')
